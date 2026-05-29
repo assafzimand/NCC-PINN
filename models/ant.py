667ec604a39f4f96dbf8e24d7fa4074108482f73
Assaf Zimand <assafzimand@gmail.com>
2026-05-29 20:43:07 +0300

    test time marching tree building

--- models/ant.py
+++ models/ant.py
@@ -39,7 +39,6 @@ class ANT(nn.Module):
         self.adaptive_config = adaptive_config
 
         self.max_experts = adaptive_config['max_experts']
-        self.max_depth = adaptive_config['max_depth']
         self.blending_mode = adaptive_config['blending_mode']
         self.sigma_fraction = adaptive_config['sigma_fraction']
         self.base_weight = adaptive_config['base_weight']
